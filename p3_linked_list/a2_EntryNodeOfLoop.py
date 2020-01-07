967f6fbe7f742295771b854d97b6dd2cbe49e984
quan <atm1992@126.com>
2020-01-07 12:17:17 +0800

    所有编程语言中的变量名都不能以数字开头

---
+++ p3_linked_list/a2_EntryNodeOfLoop.py
@@ -0,0 +1,15 @@
+#-*- coding: UTF-8 -*-
+"""
+链表中环的入口结点。
+给一个链表，若其中包含环，请找出该链表的环的入口结点，否则，输出null。
+
+解题思路：使用双指针
+"""
+
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution:
+    def EntryNodeOfLoop(self, pHead):
+        pass
